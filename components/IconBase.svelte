1f45ade70b26934d5f3a010748eb850454dad733
Ludal <44783088+iamludal@users.noreply.github.com>
2020-11-19 21:27:06 +0100

    Center icons with text
    This commit vertically aligns the icons with the text, and sets the icons width and height to 1em so that it can be used this way:
    
    <p>
      <SomeIcon />
      Some text
    </p>

--- components/IconBase.svelte
+++ components/IconBase.svelte
@@ -8,10 +8,11 @@
     stroke: currentColor;
     fill: currentColor;
     stroke-width: 0;
-    width: 100%;
+    width: 1em;
     height: auto;
-    max-height: 100%;
-  }  
+    max-height: 1em;
+    vertical-align: -0.15em;
+  } 
 </style>
 
 <svg xmlns="http://www.w3.org/2000/svg" {viewBox}>
